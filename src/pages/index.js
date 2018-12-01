547cc1144be7ad68adb9892109557640b7f485ba
TheGameFreak720 <luis.alvarez@commithub.com>
2018-12-01 13:51:57 -0500

    initialized project

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hello World</h1>
+    <h2>By Luis Alvarez</h2>
+    <p>
+      I'm a student in Lambda School and would love to attend your hackathon because it will be my 
+      first and I have always heard great things about how hackathon not only help devs get better 
+      and network but just have fun in general. Hope you consider me, thanks!
+    </p>
+  </Layout>
+)
+
+export default IndexPage
