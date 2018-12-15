a5e69a150f2ffb2ffc93314694a30f27fe7f4deb
TheGameFreak720 <luis.alvarez@commithub.com>
2018-12-15 16:22:29 -0500

    added react-hamburger menu and is styled way better than before

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,41 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { slide as Menu } from 'react-burger-menu';
 
 import './styles/header.scss';
+import logo from '../images/Medium-trans.png';
 
 const Header = ({ siteTitle }) => (
-  <div className='nav-bar'>
-    <div>
-      <Link
-        to='/'
-      >
-        <h1>{siteTitle}</h1>
-      </Link>
-    </div>
-    <div className='header-link-secondary'>
-      <Link
-        to='/'
-      >
-        Blog
-      </Link>
-      <Link
-        to='/'
-      >
-        Projects
-      </Link>
-      <Link
-        to='/'
-      >
-        Contact Me
-      </Link>
-    </div>
-  </div>
+  <Menu>
+    <Link
+      to='/'
+      className='menu-item'
+    >
+      <img src={logo} />
+    </Link>
+
+    <Link
+      to='/'
+      className='menu-item'
+    >
+      Blog
+    </Link>
+
+    <Link
+      to='/'
+      className='menu-item'
+    >
+      Projects
+    </Link>
+
+    <Link
+      to='/'
+      className='menu-item'
+    >
+      Contact Me
+    </Link>
+
+  </Menu>
 )
 
 export default Header
