ef65af4a2f4adcf85346496b83dadfcc1a0f24f9
TheGameFreak720 <luis.alvarez@commithub.com>
2018-12-23 15:56:01 -0500

    main style is done for footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,18 +11,26 @@ import { IconContext } from "react-icons";
 import './styles/footer.scss';
 
 const Footer = () => (
-    <div>
-        <p>Footer</p>
-        <div>
-            <IconContext.Provider value={{ size: '2em', className: 'twitter-btn', color: 'white'}}>
+    <div className='footer'>
+        <h2>Contact</h2>
+        <div className='btn-container'>
+            <IconContext.Provider value={{className: 'twitter-btn'}}>
                 <FaTwitter />
             </IconContext.Provider>
-            
-            <FaLinkedin />
-            <FaDev />
-            <FaGithub />
-            <FaEnvelope />
-        </div>    
+            <IconContext.Provider value={{className: 'linkedIn-btn'}}>
+                <FaLinkedin />
+            </IconContext.Provider>
+            <IconContext.Provider value={{className: 'dev-btn'}}>
+                <FaDev />
+            </IconContext.Provider>
+            <IconContext.Provider value={{className: 'github-btn'}}>
+                <FaGithub />
+            </IconContext.Provider>
+            <IconContext.Provider value={{className: 'contact-btn'}}>
+                <FaEnvelope />
+            </IconContext.Provider> 
+        </div>
+        <h4>{`${String.fromCharCode(169)} Commithub 2017`}</h4>    
     </div>
 );
 
