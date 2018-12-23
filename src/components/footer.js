b114eb4fc4b10dea8dcda4f0bdf0e52ff5c7e9c8
TheGameFreak720 <luis.alvarez@commithub.com>
2018-12-23 15:08:51 -0500

    used IconConttext to set properties for each button and created a mixin to handle params

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import { FaTwitter } from 'react-icons/fa'
+import { 
+    FaTwitter, 
+    FaLinkedin,
+    FaDev,
+    FaGithub,
+    FaEnvelope 
+} from 'react-icons/fa'
+import { IconContext } from "react-icons";
+
+import './styles/footer.scss';
 
 const Footer = () => (
     <div>
         <p>Footer</p>
         <div>
-            <FaTwitter />
+            <IconContext.Provider value={{ size: '2em', className: 'twitter-btn', color: 'white'}}>
+                <FaTwitter />
+            </IconContext.Provider>
+            
+            <FaLinkedin />
+            <FaDev />
+            <FaGithub />
+            <FaEnvelope />
         </div>    
     </div>
 );
