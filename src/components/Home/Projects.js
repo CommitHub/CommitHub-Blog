5acaceac7fe8893ad63bad04b9054ca4b5aa9292
TheGameFreak720 <luis.alvarez@commithub.com>
2018-12-27 21:35:57 -0500

    styled project section with dummy data

--- src/components/Home/Projects.js
+++ src/components/Home/Projects.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import ProjectCard from '../ProjectCard';
+import '../styles/card.scss';
 
 class Projects extends Component {
     constructor(props) {
@@ -9,22 +10,31 @@ class Projects extends Component {
             projects: [
                 {
                     id: 1,
-                    name: 'Athena',
+                    category: 'Open Source Projects',
+                    title: 'Athena',
                     description: 'An API',
+                    published: '12/27/2018',
+                    backgroundImg: 'https://opensource.com/sites/default/files/styles/image-full-size/public/lead-images/github-universe.jpg?itok=lwRZddXA',
                     technologies: ['js', 'css', 'html'],
                     url: 'https://commithub.com'
                 },
                 {
                     id: 2,
-                    name: 'Foo',
+                    category: 'Main Project',
+                    title: 'Foo',
                     description: 'An API',
+                    published: '12/27/2018',
+                    backgroundImg: 'https://opensource.com/sites/default/files/styles/image-full-size/public/lead-images/github-universe.jpg?itok=lwRZddXA',
                     technologies: ['js', 'css', 'html'],
                     url: 'https://commithub.com'
                 },
                 {
                     id: 3,
-                    name: 'Random',
+                    title: 'Random',
+                    category: 'Side Project',
                     description: 'An API',
+                    published: '12/27/2018',
+                    backgroundImg: 'https://opensource.com/sites/default/files/styles/image-full-size/public/lead-images/github-universe.jpg?itok=lwRZddXA',
                     technologies: ['js', 'css', 'html'],
                     url: 'https://commithub.com'
                 }
@@ -36,16 +46,19 @@ class Projects extends Component {
         return (
             <section>
                 <h2>Projects</h2>
-                {this.state.projects.map(project => (
-                    <section key={project.id}>
+                <section className='card-container'>
+                    {this.state.projects.map(project => (
                         <ProjectCard
-                            name={project.name}
+                            key={project.id}
+                            title={project.title}
                             description={project.description}
+                            published={project.published}
+                            backgroundImg={project.backgroundImg}
                             technologies={project.description}
                             url={project.url}
                         />
-                    </section>
-                ))}
+                    ))}
+                </section>
             </section>
         );
     }
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const ProjectCard = (props) => (
-    <section>
-        <h3>{props.name}</h3>
-        <p>{props.description}</p>
-        <a href={props.url}>url</a>
+    <section className='card'>
+        <img src={props.backgroundImg} alt='project background' />
+        <section className='card-description-container'>
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+            <p>{props.published}</p>
+            <a href={props.url}>url</a>
+        </section>
     </section>
 )
 
