9517223695b62e670c7156230572df5f282e33bc
TheGameFreak720 <lalvarez@docnetwork.org>
2019-06-14 10:04:33 -0400

    404 page is alive

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
+import { Link } from 'gatsby';
 import { slide as Menu } from 'react-burger-menu';
 
 import './styles/header.scss';
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React from 'react';
+import Layout from '../components/layout';
+import './styles/404.scss';
+import notFoundLogo from '../images/404/404.gif';
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section class="not-found-container">
+      <img
+        src={notFoundLogo}
+        alt="404"
+      />
+      <p>
+        You gone and done it. You are in empty cyberspace.
+        You might want to head back to home using that fancy navbar on the top left or don't and stay stuck in here forever.
+      </p>
+    </section>
   </Layout>
 )
 
