6f7734847a8cc64cba92d42cb93e20ef5a4d1715
Luis Alvarez <luis.alvarez@commithub.com>
2019-09-14 11:44:54 -0400

    resized favicon - Added icon folder with an image 144x144 - Added icon to the manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,13 @@ module.exports = {
         theme_color: '#21c6ce',
         display: 'minimal-ui',
         icon: 'src/images/logo/favicon.png', // This path is relative to the root of the site.
+        icons: [
+          {
+            src: `/icons/icon-144x144.png`,
+            sizes: `144x144`,
+            type: `image/png`,
+          },
+        ]
       },
     },
     'gatsby-plugin-offline',
